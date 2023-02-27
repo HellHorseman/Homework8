ebd3a5c5961ff6f55574f70d91eb76209aa4fa57
HellHorseman <duke.sheffer@yandex.ru>
2023-02-27 20:22:55 +0400

    Start new branch + add task 1+2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task3();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1 и 2");
+    int massive [] = new int [3];
+    massive [0] = 1;
+    massive [1] = 2;
+    massive [2] = 3;
+        System.out.print(massive[0] + ", " + massive[1] + ", " + massive[2]);
+        System.out.println();
+    double massive1 [] = {1.57, 7.654, 9.986};
+        System.out.print(massive1[0] + ", " + massive1[1] + ", " + massive1[2]);
+        System.out.println();
+    int year [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        System.out.print(year[0] + ", " + year[1] + ", " + year[2] + ", " + year[3] + ", " + year[4] + ", " + year[5] + ", " + year[6] + ", " + year[7] + ", " + year[8] + ", " + year[9] + ", " + year[10] + ", " + year[11]);
+        System.out.println();
+    }
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+
     }
 }
